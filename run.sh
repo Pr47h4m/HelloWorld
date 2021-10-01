c53dcd0030fc53673ead90b564da7ec8f2c15ab5
Pratham Jaiswal <pr47h4m@gmail.com>
2021-10-01 10:38:26 +0530

    run.sh

---
+++ run.sh
@@ -0,0 +1,31 @@
+echo "compiling C program using command 'gcc helloworld.c'"
+gcc helloworld.c
+echo "running C program using command './a.out'"
+echo 
+./a.out
+echo 
+echo "compiling C++ program using command 'g++ helloworld.cpp'"
+g++ helloworld.cpp
+echo "running C++ program using command './a.out'"
+echo 
+./a.out
+echo 
+echo "compiling and running Java program using command 'java helloworld.java'"
+echo 
+java helloworld.java
+echo 
+echo "running JavaScript program using command 'node helloworld.js'"
+echo 
+node helloworld.js
+echo 
+echo "Running Python program using command 'python3 helloworld.py'"
+echo
+python3 helloworld.py
+echo 
+echo "running SH program using command 'sh helloworld.sh'"
+echo 
+sh helloworld.sh
+echo 
+echo "running Docker whalesay using command 'sudo docker run docker/whalesay cowsay \"Hello World\"'"
+echo 
+sudo docker run docker/whalesay cowsay "Hello World"
